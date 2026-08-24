f952dd42cfd5d46deb462740e135d358430d9ee9
jonathanjimeneza343-source <jonathanjimeneza343@gmail.com>
2026-08-24 11:22:04 -0500

    Segundo commit (commonjs) levantamiento del servidor Tradicional

---
+++ App.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+const result = dotenv.config();
+
+if (result.error) {
+  console.log('No se pudo cargar el archivo .env:', result.error.message);
+} else {
+  console.log('.env cargado correctamente:', result.parsed);
+}
+
+const app = express();
+const PUERTO = process.env.MIPUERTO || 3003;
+
+app.get('/', (req, res) => {
+  res.send('API Rest Full con express');
+});
+
+app.listen(PUERTO, () => {
+  console.log(`API Rest Full con express - Puerto ${PUERTO}`);
+});
