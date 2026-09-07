16d476b048ad52e1e457eec1146b2100dc5daaf0
jonathanjimeneza343-source <jonathanjimeneza343@gmail.com>
2026-09-07 11:45:45 -0500

    Proyecto-crud-astrid

--- App.js
+++ App.js
@@ -1,21 +1,135 @@
 const express = require('express');
-const dotenv = require('dotenv');
 
-const result = dotenv.config();
+// Librerías fs, path
+const sistemaArchivos = require("fs");
+const ruta = require("path");
 
-if (result.error) {
-  console.log('No se pudo cargar el archivo .env:', result.error.message);
-} else {
-  console.log('.env cargado correctamente:', result.parsed);
-}
 
-const app = express();
-const PUERTO = process.env.MIPUERTO || 3003;
+const MIPUERTO = 3333;
+const rutaMiArchivo = ruta.join(__dirname, "datos.json");
 
-app.get('/', (req, res) => {
-  res.send('API Rest Full con express');
+//importar Multer
+const multer = require ("multer")
+
+// Almacenamiento
+const almacen = multer.diskStorage({
+    destination: (req, File, cb) => {
+        cb(null, "misImagenes/");
+    },
+
+    filename: (req, File, cb) => {
+        const extension = ruta.extname(File.originalname);
+
+        cb(null, `${Date.now()} ${extension}`);
+    }
 });
 
-app.listen(PUERTO, () => {
-  console.log(`API Rest Full con express - Puerto ${PUERTO}`);
+
+const subir = multer ({ storage: almacen})
+
+
+// Middleware body-parser
+const app = express ()
+app.use(express.json());
+app.use (express.urlencoded({extended: true}))
+
+
+// Ruta principal
+app.get("/", (_, res) => {
+    res.send('API REST Full con Express');
+});
+
+
+// Obtener lista de aprendices desde el archivo
+app.get("/api/aprendices", (req, res) => {
+
+    sistemaArchivos.readFile(rutaMiArchivo, "UTF-8", (error, datos) => {
+
+        if (error) {
+            res.status(500).json({
+                Error: "No se puede leer el archivo"
+            });
+        }
+
+        const ListaAprendices = JSON.parse(datos);
+
+        res.status(200).json({
+            Listado: ListaAprendices
+        });
+
+    });
+
+});
+
+
+// Obtener aprendices
+app.get("/api/aprendices", (_, res) => {
+    res.status(200).json({
+        mensaje: 'lista aprendices'
+    });
+});
+
+
+// Crear aprendiz
+app.post("/api/aprendices", subir.single ('imagen'),(req, res) => {
+
+    const datosAprendiz = req.body;
+    datosAprendiz.imagen = req.file? `/misImagenes/ ${req.file.filename}` : "sin imagen"
+
+    sistemaArchivos.readFile(rutaMiArchivo, "UTF-8", (error, datos) => {
+
+        if (error) {
+            res.status(500).json({
+                Error: "No se puede leer el archivo"
+            });
+        }
+
+        const ListaAprendices = JSON.parse(datos);
+
+        ListaAprendices.push(datosAprendiz);
+
+        sistemaArchivos.writeFile(
+            rutaMiArchivo,
+            JSON.stringify(ListaAprendices, null, 2),
+            (error) => {
+
+                if (error) {
+                    res.status(500).json({
+                        Error: "No se puede escribir en file"
+                    });
+                }
+
+                res.status(200).json({
+                    Mensaje: "Creado",
+                    Datos: datosAprendiz
+                });
+
+            }
+        );
+
+    });
+
+});
+
+
+
+// Actualizar aprendiz
+app.put("/api/aprendices/:id_aprendices", (_, res) => {
+    res.status(200).json({
+        mesaje: 'Actializar aprendiz'
+    });
+});
+
+
+// Eliminar aprendiz
+app.delete("/api/aprendices/:id_aprendices", (_, res) => {
+    res.status(200).json({
+        mensaje: 'Eliminada'
+    });
+});
+
+
+// Iniciar servidor
+app.listen(MIPUERTO, () => {
+    console.log(`Servidor en funcionamiento en el puerto: ${MIPUERTO}`);
 });
